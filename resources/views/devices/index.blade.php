2c49c266e0996112d9821e235bb1e8ec3a823b96
HARIS RAHMAN NASUTION <haris.nasution@binus.ac.id>
2023-08-21 16:45:27 +0700

    perbaikan

--- resources/views/devices/index.blade.php
+++ resources/views/devices/index.blade.php
@@ -39,9 +39,6 @@
                                     Rack
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    QR Code
-                                </th>
-                                <th scope="col" class="px-6 py-3">
 
                                 </th>
 
@@ -71,9 +68,7 @@
                                 <td class="px-6 py-4">
                                     10
                                 </td>
-                                <td class="px-6 py-4">
-                                    CODE
-                                </td>
+
                                 <td class="px-6 py-4">
                                     <button type="button"
                                         class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Detail</button>
@@ -107,9 +102,6 @@
                                     10
                                 </td>
                                 <td class="px-6 py-4">
-                                    CODE
-                                </td>
-                                <td class="px-6 py-4">
                                     <button type="button"
                                         class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Detail</button>
                                     <button type="button"
@@ -141,9 +133,7 @@
                                 <td class="px-6 py-4">
                                     10
                                 </td>
-                                <td class="px-6 py-4">
-                                    CODE
-                                </td>
+
                                 <td class="px-6 py-4">
                                     <button type="button"
                                         class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Detail</button>
