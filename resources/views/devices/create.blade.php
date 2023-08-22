378406c21299ad195a0d1b196272998f657a99d3
HARIS RAHMAN NASUTION <haris.nasution@binus.ac.id>
2023-08-22 17:26:26 +0700

    update input

--- resources/views/devices/create.blade.php
+++ resources/views/devices/create.blade.php
@@ -108,12 +108,15 @@
                 </div>
 
                 <div class="mb-6">
-                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                        for="device_image">Upload
+
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="device_name">Upload
                         file</label>
-                    <input name="device_image"
+                    <input
                         class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                        aria-describedby="device_image" id="device_image" type="file">
+                        aria-describedby="device_name_help" id="device_name" name="device_name" type="file">
+                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="device_name_help">SVG, PNG, JPG or GIF
+                        (MAX. 800x400px).</p>
+
                 </div>
 
                 <div class="mb-6">
