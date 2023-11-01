997cc4e13c48e4e97dde5474571af07d9311936d
HARIS RAHMAN NASUTION <haris.nasution@binus.ac.id>
2023-11-01 14:43:36 +0700

    update Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\device;
+use App\Models\rack;
+use App\Models\site;
+use App\Models\location;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return redirect()->route('dashboard');
+        $deviceCount = Device::count();
+        $rackCount  = rack::count();
+        $siteCount  = site::count();
+        $locationCount  = location::count();
+        // return redirect()->route('dashboard')->with('deviceCount', $deviceCount);
+        return view('dashboard', compact('deviceCount','rackCount','siteCount','locationCount'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,96 +6,83 @@
     </x-slot>
 
     <div class="py-2">
-        <div class="p-4 sm:ml-64">
+        <div class="p-4 sm:ml-0">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
                 <section class="bg-white dark:bg-gray-900">
                     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-                        <div
-                            class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-                            <a href="#"
-                                class="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-blue-400 mb-2">
-                                <svg class="w-2.5 h-2.5 mr-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="currentColor" viewBox="0 0 20 14">
-                                    <path
-                                        d="M11 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm8.585 1.189a.994.994 0 0 0-.9-.138l-2.965.983a1 1 0 0 0-.685.949v8a1 1 0 0 0 .675.946l2.965 1.02a1.013 1.013 0 0 0 1.032-.242A1 1 0 0 0 20 12V2a1 1 0 0 0-.415-.811Z" />
-                                </svg>
-                                Tutorial
-                            </a>
-                            <h1 class="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">How to
-                                quickly deploy a static website</h1>
-                            <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">Static websites are now
-                                used to bootstrap lots of websites and are becoming the basis for a variety of tools
-                                that even influence both web designers and developers.</p>
-                            <a href="#"
-                                class="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                                Read more
-                                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </a>
-                        </div>
-                        <div class="grid md:grid-cols-2 gap-8">
-                            <div
-                                class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+
+
+                        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+                            <div>
                                 <a href="#"
-                                    class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
-                                    <svg class="w-2.5 h-2.5 mr-1.5" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                                        <path
-                                            d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
-                                    </svg>
-                                    Design
-                                </a>
-                                <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Start with
-                                    Flowbite Design System</h2>
-                                <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Static websites are
-                                    now used to bootstrap lots of websites and are becoming the basis for a variety of
-                                    tools that even influence both web designers and developers.</p>
-                                <a href="#"
-                                    class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read
-                                    more
-                                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                        fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
+                                    class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        Total Device</h5>
+                                    <dt class="mb-2 text-3xl font-extrabold">{{ $deviceCount }}</dt>
                                 </a>
                             </div>
-                            <div
-                                class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+
+                            <div>
+
                                 <a href="#"
-                                    class="bg-purple-100 text-purple-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-purple-400 mb-2">
-                                    <svg class="w-2.5 h-2.5 mr-1.5" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15" />
-                                    </svg>
-                                    Code
+                                    class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        Total Racks</h5>
+                                    <dt class="mb-2 text-3xl font-extrabold">{{ $rackCount }}</dt>
                                 </a>
-                                <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Best react
-                                    libraries around the web</h2>
-                                <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Static websites are
-                                    now used to bootstrap lots of websites and are becoming the basis for a variety of
-                                    tools that even influence both web designers and developers.</p>
+
+                            </div>
+
+                            <div>
+
                                 <a href="#"
-                                    class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Read
-                                    more
-                                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                        fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
+                                    class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        Total Site</h5>
+                                    <dt class="mb-2 text-3xl font-extrabold">{{ $siteCount }}</dt>
                                 </a>
+
+                            </div>
+                            <div>
+
+                                <a href="#"
+                                    class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        Total Location</h5>
+                                    <dt class="mb-2 text-3xl font-extrabold">{{ $locationCount }}</dt>
+                                </a>
+
                             </div>
                         </div>
                     </div>
+
                 </section>
 
             </div>
         </div>
+
+        <figure class="max-w-screen-md mx-auto text-center">
+            <svg class="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                <path
+                    d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+            </svg>
+            <blockquote>
+                <p class="text-2xl italic font-medium text-gray-900 dark:text-white">"Memberikan layanan e-Business yang
+                    inovatif, berstandar internasional dengan menjunjung tinggi Good Corporate Governance untuk
+                    memberikan nilai tambah berkelanjutan bagi stakeholder."</p>
+            </blockquote>
+            <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                <img class="w-6 h-6 rounded-full" src="https://www.svgrepo.com/show/223967/profits-statistics.svg"
+                    alt="profile picture">
+                <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                    <cite class="pr-3 font-medium text-gray-900 dark:text-white">PT. EDI Indonesia</cite>
+                    <cite class="pl-3 text-sm text-gray-500 dark:text-gray-400">Bina Nusantara</cite>
+                </div>
+            </figcaption>
+        </figure>
+
     </div>
 
 </x-app-layout>
