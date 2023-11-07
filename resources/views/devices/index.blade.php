4a77339933f2468fe5ddb630a197a2ce022416de
HARIS RAHMAN NASUTION <haris.nasution@binus.ac.id>
2023-11-07 22:36:54 +0700

    rubah tulisan salah

--- resources/views/devices/index.blade.php
+++ resources/views/devices/index.blade.php
@@ -107,7 +107,7 @@
 
                                         <a href="{{ route('device.destroy', $device->id) }}"
                                             class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                                            onclick="event.preventDefault(); if (confirm('Are you sure you want to delete this Site?')) document.getElementById('delete-form-{{ $device->id }}').submit();">
+                                            onclick="event.preventDefault(); if (confirm('Are you sure you want to delete this Device?')) document.getElementById('delete-form-{{ $device->id }}').submit();">
                                             Delete
                                         </a>
 
