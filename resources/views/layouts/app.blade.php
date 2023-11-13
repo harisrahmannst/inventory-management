07d4c1bb9a0915e36d2a58b447693613d3482159
Rahmat Hidayat <--unset>
2023-11-13 15:24:52 +0700

    - fix footer to suit with content - fix link to PT EDI website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,25 +48,27 @@
     {{-- Footer --}}
 
     <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/"
-                    class="hover:underline">PT. EDI Indonesia </a>. All Rights Reserved.
-            </span>
-            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                <li>
-                    <a href="https://edi-indonesia.co.id/id/about-us/" class="mr-4 hover:underline md:mr-6 ">About</a>
-                </li>
-                <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="https://edi-indonesia.co.id/id/sertifikat-edii/"
-                        class="mr-4 hover:underline md:mr-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="https://edi-indonesia.co.id/id/contact-us/" class="hover:underline">Contact</a>
-                </li>
-            </ul>
+        <div class="p-4 sm:ml-64 mx-auto py-6 lg:px-8">
+            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://edi-indonesia.co.id/"
+                        class="hover:underline">PT. EDI Indonesia </a>. All Rights Reserved.
+                </span>
+                <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a href="https://edi-indonesia.co.id/id/about-us/" class="mr-4 hover:underline md:mr-6 ">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="https://edi-indonesia.co.id/id/sertifikat-edii/"
+                            class="mr-4 hover:underline md:mr-6">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="https://edi-indonesia.co.id/id/contact-us/" class="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </footer>
 
