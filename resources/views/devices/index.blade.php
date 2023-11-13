7b9dfb778f8fd3fc0dcf296a893f0b8262cff1bb
Rahmat Hidayat <--unset>
2023-11-13 15:23:46 +0700

    fix overlaping action buttons on device index

--- resources/views/devices/index.blade.php
+++ resources/views/devices/index.blade.php
@@ -59,7 +59,7 @@
                                     Status
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-
+                                    Action
                                 </th>
 
                             </tr>
@@ -94,29 +94,30 @@
                                     </td>
 
                                     <td class="px-6 py-4">
+                                        <div class="flex space-x-2">
+                                            <a href="{{ route('device.show', $device->id) }}"
+                                                class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                                Detail
+                                            </a>
 
-                                        <a href="{{ route('device.show', $device->id) }}"
-                                            class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                                            Detail
-                                        </a>
+                                            <a href="{{ route('device.edit', $device->id) }}"
+                                                class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
+                                                Edit
+                                            </a>
 
-                                        <a href="{{ route('device.edit', $device->id) }}"
-                                            class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
-                                            Edit
-                                        </a>
-
-                                        <a href="{{ route('device.destroy', $device->id) }}"
-                                            class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                                            onclick="event.preventDefault(); if (confirm('Are you sure you want to delete this Device?')) document.getElementById('delete-form-{{ $device->id }}').submit();">
-                                            Delete
-                                        </a>
-
+                                            <a href="{{ route('device.destroy', $device->id) }}"
+                                                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                                onclick="event.preventDefault(); if (confirm('Are you sure you want to delete this Device?')) document.getElementById('delete-form-{{ $device->id }}').submit();">
+                                                Delete
+                                            </a>
+                                        </div>
+                                        
                                         <form id="delete-form-{{ $device->id }}"
                                             action="{{ route('device.destroy', $device->id) }}" method="POST"
                                             style="display: none;">
                                             @csrf
                                             @method('DELETE')
-                                        </form>
+                                        </form>                                        
                                     </td>
                                 </tr>
                             @endforeach
